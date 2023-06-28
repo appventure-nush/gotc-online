efc9cb4566f76fd87cee05ed5a1d5278d954f85c
OrangeRectanglePerson <orangerectangleperson@gmail.com>
2023-06-29 02:27:15 +0800

    added auto sign out due to inactivity [F&B]

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import time
+import threading
 from datetime import datetime
 from flask import Flask, request, send_file
 from flask_cors import CORS, cross_origin
@@ -17,6 +19,27 @@ class User:
 
     def __str__(self):
         return self.name + " | " + str(self.last_checkin)
+
+def printUsers():
+    for i in logged_in:
+        print(i)
+
+def clear_inactive_users_from_login():
+    while True:
+        users_to_log_off = []
+        curr_time = int(datetime.now().timestamp())
+        for i in logged_in:
+            if(curr_time - i.last_checkin > 60):  #if not checked in for more than 10*60 secs = 10 mins
+                users_to_log_off.append(i)
+        for i in users_to_log_off:
+            logged_in.remove(i)
+        print("after clearing cycle @" + str(curr_time) + ":")
+        printUsers()
+        print("===")
+        time.sleep(30) # do the check ever 2 mins
+
+log_in_clearer_daemon = threading.Thread(target=clear_inactive_users_from_login, daemon=True)
+log_in_clearer_daemon.start()
 
 
 @app.route('/')
@@ -77,9 +100,48 @@ def sign_in():
         printUsers()
         return response
 
-def printUsers():
-    for i in logged_in:
-        print(i)
+@app.route('/user_activity_ping',methods=['POST'])
+#we need this cross-origin stuff for post requests since this is how people decided the internet would work
+@cross_origin()
+def user_activity_ping():
+    pinger_username = request.json['username']
+    response = {
+        "text" : "PLACEHOLDER",
+        "still_active" : False
+    }
+    if request.method == "POST":
+        for i in logged_in:
+            if i.name == pinger_username:
+                i.last_checkin = int(datetime.now().timestamp())
+                response["text"] = "activity sucessfully registered"
+                response["still_active"] = True
+                return response
+        #else
+        response["text"] = "You were signed out due to inactivity!"
+        response["still_active"] = False
+        return response
+
+@app.route('/activity_status_request',methods=['POST'])
+#we need this cross-origin stuff for post requests since this is how people decided the internet would work
+@cross_origin()
+def activity_status_request():
+    pinger_username = request.json['username']
+    response = {
+        "text" : "PLACEHOLDER",
+        "still_active" : False
+    }
+    if request.method == "POST":
+        for i in logged_in:
+            if i.name == pinger_username:
+                response["text"] = "account still logged in"
+                response["still_active"] = True
+                return response
+        #else
+        response["text"] = "You were signed out due to inactivity!"
+        response["still_active"] = False
+        return response
+
+
 
 @app.route('/get_image')
 def get_image():
--- frontend/src/userform.ts
+++ frontend/src/userform.ts
@@ -3,18 +3,27 @@ let BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 export function setupUserForm(element: HTMLDivElement) {
     let result = "waiting for input"
-    // @ts-ignore
     let curr_user = ""
+    // @ts-ignore
+    let activity_pinger_id : number = 0
     element.innerHTML = `
-<p style="border: white; border-width: 5px" id = "userform_status_top">sign in:</p>
+<p style="border: white; border-width: 5px" id = "userform_status_top">Sign In:</p>
 <input type="text" id = "username_textin" value="" placeholder="enter username">
 <button type="submit" id = "userform_butt">sign in</button>
 <p style="border: white; border-width: 5px" id = "userform_status_bottom">${result}</p>
 `
     element.children.namedItem("userform_butt")!.addEventListener(
         "click",
-        () => submit(String((element.children.namedItem("username_textin") as HTMLInputElement).value))
+        () => {
+            let proposed_name = String((element.children.namedItem("username_textin") as HTMLInputElement).value)
+            if (proposed_name === ""){
+                element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = "Please enter a username"
+            }
+            else submit(proposed_name)
+        }
     )
+
+
 
     const submit = (value : string) => {
         element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = "sending sign in request..."
@@ -43,6 +52,7 @@ export function setupUserForm(element: HTMLDivElement) {
                 if(json_response["login_success"] === true) {
                     curr_user = json_response["confirmed_username"]
                     refreshText(json_response)
+                    activity_pinger_id = setInterval(activity_ping, 20_000)
                 }
                 else {
                     refreshText(json_response)
@@ -53,6 +63,90 @@ export function setupUserForm(element: HTMLDivElement) {
                 refreshText(error)
             });
     }
+
+    const activity_ping = () => {
+        if(curr_user !== "" && window.document.hasFocus()) {
+            element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = "pinging for activity..."
+            // send a post request to the calculate part of the backend
+            // body of post request is a json
+            // first 3 parts of json are just fluff, the actual thing that matters now is the value : value part
+            fetch(`${BACKEND_URL}/user_activity_ping`, {
+                method: "POST",
+                body: JSON.stringify({
+                    username: curr_user
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+                // this results in a response promise that promises some sort of response
+                // will be received from where the post was sent to
+
+
+                .then((response) => {
+                    if (!response.ok) return Promise.reject(response)
+                    else return response.text()
+                })
+                .then((json_text) => {
+                    let json_response = JSON.parse(json_text)
+                    if (json_response["still_active"] === true) {
+                        element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = json_response["text"]
+                    } else {
+                        clearInterval(activity_pinger_id)
+                        curr_user = ""
+                        element.querySelector<HTMLParagraphElement>("#userform_status_top")!.innerHTML = "Sign In:"
+                        element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = json_response["text"]
+                    }
+                })
+
+                .catch(error => {
+                    refreshText(error)
+                });
+        }
+    }
+
+    const check_if_still_signed_in = () => {
+        if(curr_user !== "") {
+            element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = "pinging for activity..."
+            // send a post request to the calculate part of the backend
+            // body of post request is a json
+            // first 3 parts of json are just fluff, the actual thing that matters now is the value : value part
+            fetch(`${BACKEND_URL}/activity_status_request`, {
+                method: "POST",
+                body: JSON.stringify({
+                    username: curr_user
+                }),
+                headers: {
+                    "Content-type": "application/json; charset=UTF-8"
+                }
+            })
+                // this results in a response promise that promises some sort of response
+                // will be received from where the post was sent to
+
+
+                .then((response) => {
+                    if (!response.ok) return Promise.reject(response)
+                    else return response.text()
+                })
+                .then((json_text) => {
+                    let json_response = JSON.parse(json_text)
+                    if (json_response["still_active"] === true) {
+                        element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = json_response["text"]
+                    } else {
+                        clearInterval(activity_pinger_id)
+                        curr_user = ""
+                        element.querySelector<HTMLParagraphElement>("#userform_status_top")!.innerHTML = "Sign In:"
+                        element.querySelector<HTMLParagraphElement>("#userform_status_bottom")!.innerHTML = json_response["text"]
+                    }
+                })
+
+                .catch(error => {
+                    refreshText(error)
+                });
+        }
+    }
+
+    window.document.onfocus = check_if_still_signed_in
 
     const refreshText = (json_response : any) => {
         // update result variable and result element
