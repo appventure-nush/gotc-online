045ca0de99d80c7e1c84fb95832f390e7367685a
OrangeRectanglePerson <orangerectangleperson@gmail.com>
2023-08-14 18:16:36 +0800

    Updated Mainpage appearance (not functionality) to proposed plan.[F]

--- frontend/src/Pages/MainPage/MainPageApp.vue
+++ frontend/src/Pages/MainPage/MainPageApp.vue
@@ -1,22 +1,77 @@
 <script setup lang="ts">
 
+import TopBarButtonBar from "./components/topbarbuttonbar.vue";
+import Topbarbuttonbar from "./components/topbarbuttonbar.vue";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
 
 </script>
 
 <template>
   <div class="all_container">
-
+    <div class="topbar">
+      <p class="gotc-online-pseudologo">GOTC<br>ONLINE</p>
+      <topbarbuttonbar/>
+      <button>Sign In</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
+
+
+
+.topbar .gotc-online-pseudologo{
+  text-align: center;
+  color: #c1f773ff;
+  font-size: 2.5vh;
+  font-weight: 600;
+}
+
+:deep button {
+  border-radius: 10pt;
+  background-color: #d9c779ff; /* Green background */
+  border: 5px solid #cd931bff; /* Green border */
+  color: black; /* White text */
+  padding: 0.5vh 0.5vw; /* Some padding */
+  cursor: pointer; /* Pointer/hand icon */
+  float: left; /* Float the buttons side by side */
+
+  width: 120pt ;
+  text-align: center;
+
+  font-size: 3vh;
+
+  transition: border-color 0.3s;
+}
+
+:deep button:hover{
+  border-color: firebrick;
+}
+
+
 
 .all_container{
   background-image: url("../../assets/mainpage_bg.png");
   background-size: 100vw 100vh;
   height: 100vh;
   width: 100vw;
+
+  font-family: "Exo 2",serif;
 }
+
+.topbar{
+  height: 8vh;
+  background-color: #964e44ff;
+  top: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  padding-left: 10vw;
+  padding-right: 10vw;
+}
+
 
 </style>
---
+++ frontend/src/Pages/MainPage/components/topbarbuttonbar.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="btn-group">
+    <button>HELP</button>
+    <button>REPLAY</button>
+    <button>LADDER</button>
+    <button class="play_b">PLAY</button>
+  </div>
+</template>
+
+<style scoped>
+
+.btn-group button {
+  border-radius: 0;
+}
+
+.btn-group button:not(:last-child, :first-child) {
+  border-radius: 0;
+}
+
+.btn-group button:not(:last-child,) {
+  border-right: none; /* Prevent double borders */
+}
+
+.btn-group button:first-child {
+  border-top-left-radius: 10pt;
+  border-bottom-left-radius: 10pt;
+}
+
+.btn-group button:last-child {
+  border-top-right-radius: 10pt;
+  border-bottom-right-radius: 10pt;
+}
+
+/* Clear floats (clearfix hack) */
+.btn-group:after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.play_b {
+  border-color: #9ed813ff !important;
+
+  transition: border-color 0.3s !important;
+}
+
+.play_b:hover{
+  border-color: aquamarine !important;
+}
+</style>
