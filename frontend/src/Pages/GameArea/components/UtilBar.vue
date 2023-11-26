c2ff7d282d48b2a83d6b3e5de59d59ece27d903d
OrangeRectanglePerson <orangerectangleperson@gmail.com>
2023-11-26 16:46:49 +0800

    Added Crisis and Defence card sections. [F&B]
    Also:
    - Backend hosts crisis cards.
    - General structure of Player's area is done, although still both aesthetically and functionally incomplete.

---
+++ frontend/src/Pages/GameArea/components/UtilBar.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "UtilBar"
+})
+</script>
+
+<template>
+  <div class="utilbar-component-div" >
+    <p>UTIL/TOOLBAR</p>
+  </div>
+</template>
+
+<style scoped>
+
+.utilbar-component-div{
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-evenly;
+  background-color: rgb(128,128,128,0.75);
+}
+
+</style>
