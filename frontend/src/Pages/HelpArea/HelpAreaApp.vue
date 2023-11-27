0b82de4adb49b608739206e9a13c757f44999f1c
Kieran Cheng <87019594+PythonRocks1234@users.noreply.github.com>
2023-11-27 16:16:52 +0800

    add help area route [F]

---
+++ frontend/src/Pages/HelpArea/HelpAreaApp.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "HelpAreaApp"
+})
+</script>
+
+<template>
+  <p>Hello world</p>
+</template>
+
+<style scoped>
+
+</style>
