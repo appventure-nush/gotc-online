77ceb55e091bc78624c7e555616722e0998ecb5c
Kieran Cheng <87019594+PythonRocks1234@users.noreply.github.com>
2023-11-30 11:07:42 +0800

    Added a description of the rules [F]

--- frontend/src/Pages/HelpArea/HelpAreaApp.vue
+++ frontend/src/Pages/HelpArea/HelpAreaApp.vue
@@ -18,6 +18,15 @@ export default defineComponent({
       GOTC-online is an online version of GOTC, allowing for online play with friends and saving of replays.
       <hr style="border-top: 1px solid rgb(204, 204, 204);">
       <h3>How to play</h3>
+      The objective is to remove your Crisis card, by displaying the required defences shown on the card.<br>
+      Each player picks 1 random Crisis card. If both players have the same Crisis card, shuffle and repick again.<br>
+      The first player will draw 4 cards, while the other player will draw 5 cards.<br><br>
+      During each turn, the player will draw 1 card from the Draw Pile and may play 1 card from their hand.<br>
+      Read the text from the card and follow the instructions on the card accordingly. Some cards may have additional effects activated only when you have enough Community Support, or your opponent does not have enough Community Support.<br>
+      At the end of each turn, players can only hold a maximum of 7 cards in their hand. If you have more than 7 cards at the end of your turn, you must discard additional cards until you have 7 cards.<br><br>
+      You win when you display all the required defences on your Crisis card.<br>
+      If no one has won when the timer has run out or a player has run out of cards to draw, players will continue playing in order until the turn goes back to the first player. The first player does not play again, because the game ends.<br>
+      Then, the player that displayed the most required defence cards wins, not counting duplicates.<br>
     </div>
   </div>
 </template>
